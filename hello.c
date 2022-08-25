4be8fa387ec03bf38358ebc27d7df00019af6ccb
justinkrutz <agentrabbit64@gmail.com>
2022-08-25 19:32:11 +0000

    added hello world code

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Justin Krutz
+ * Date: 2022/08/25
+ *
+ * Hack 1.0
+ *
+ */
+
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+    printf("Hello World, I'm Justin Krutz!\n");
+
+    return 0;
+}
